787f752eb207e56b4a0bf6de1d15e8915dee2172
Roman Volodin <roman.volodin@gmail.com>
2021-01-27 17:58:21 +0300

    Add API endpoint for Places

--- places/views.py
+++ places/views.py
@@ -1,9 +1,53 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
+from django.shortcuts import get_object_or_404
+from .models import (
+    Place,
+)
 
 
 def index(request):
+    places = Place.objects.all()
+
+    places_data = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [place.longitude, place.latitude]
+                },
+                "properties": {
+                    "title": place.title,
+                    "placeId": place.place_id,
+                    "detailsUrl": place.details_url
+                }
+            } for place in places
+        ]
+    }
+    
     template = loader.get_template('index.html')
-    context = {}
+    context = {"places_data": places_data}
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)
+
+
+def place_detail(request, place_id):
+    place = get_object_or_404(Place, pk=place_id)
+    return JsonResponse(
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.longitude, place.latitude]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.place_id,
+                "detailsUrl": place.details_url
+            }
+        },
+        safe=False,
+        json_dumps_params={'ensure_ascii': False, 'indent': 2}
+    )
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -7,7 +7,8 @@ from places import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index),
+    path('places/<int:place_id>/', views.place_detail),
 ]
 
 if settings.DEBUG:
