c60740e9d239a1c4b05e5afefbf2df3494d9a908
Roman Volodin <roman.volodin@gmail.com>
2021-01-28 00:16:27 +0300

    Set STATIC_ROOT

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'assets'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
