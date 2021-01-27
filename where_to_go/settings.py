f4a6d2a21aea07ac3dce2beb7cbc952fe970cc4b
Roman Volodin <roman.volodin@gmail.com>
2021-01-28 00:03:32 +0300

    Env defaults for easier test deploy

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY = env.str("SECRET_KEY", default='SEKRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
