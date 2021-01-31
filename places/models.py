913b8ac6e5bbe1b7b5c4248813de5f0fd12f50ef
Roman Volodin <roman.volodin@gmail.com>
2021-01-31 23:41:54 +0300

    Add verbose_name in Place and Image models.

--- places/models.py
+++ places/models.py
@@ -3,21 +3,21 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=255)
-    place_id = models.CharField(max_length=255, blank=True)
+    title = models.CharField(verbose_name="заголовок", max_length=255)
+    place_id = models.CharField(verbose_name="id места", max_length=255, blank=True)
     longitude = models.FloatField(verbose_name="долгота", default=0)
     latitude = models.FloatField(verbose_name="широта", default=0)
-    description_short = models.TextField(blank=True)
-    description_long = HTMLField(blank=True)
+    description_short = models.TextField(verbose_name="Короткое описание", blank=True)
+    description_long = HTMLField(verbose_name="Длинное описание", blank=True)
 
     def __str__(self):
         return self.title
 
 
 class Image(models.Model):
-    place = models.ForeignKey(Place, default=0, on_delete=models.CASCADE)
-    order = models.PositiveIntegerField(default=0)
-    image = models.ImageField()
+    place = models.ForeignKey(Place, default=0, on_delete=models.CASCADE, verbose_name="место")
+    order = models.PositiveIntegerField(default=0, verbose_name="номер по порядку")
+    image = models.ImageField(verbose_name="картинка")
 
     class Meta:
         ordering = ('order',)
