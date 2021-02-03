da44bdf5abd57583e1c5c1b9c5f0f7537f7c64d9
Roman Volodin <roman.volodin@gmail.com>
2021-02-03 16:15:45 +0300

    Remove default for some fields, since they are not reasonable.

--- places/models.py
+++ places/models.py
@@ -7,8 +7,8 @@ from tinymce.models import HTMLField
 class Place(models.Model):
     title = models.CharField(verbose_name="заголовок", max_length=255)
     place_id = models.CharField(verbose_name="id места", max_length=255, blank=True, default=uuid.uuid1)
-    longitude = models.FloatField(verbose_name="долгота", default=0)
-    latitude = models.FloatField(verbose_name="широта", default=0)
+    longitude = models.FloatField(verbose_name="долгота")
+    latitude = models.FloatField(verbose_name="широта")
     short_description = models.TextField(verbose_name="Короткое описание", blank=True)
     long_description = HTMLField(verbose_name="Длинное описание", blank=True)
 
@@ -17,7 +17,7 @@ class Place(models.Model):
 
 
 class Image(models.Model):
-    place = models.ForeignKey(Place, default=0, on_delete=models.CASCADE, verbose_name="место", related_name='images')
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, verbose_name="место", related_name='images')
     order = models.PositiveIntegerField(default=0, verbose_name="номер по порядку")
     image = models.ImageField(verbose_name="картинка")
 
